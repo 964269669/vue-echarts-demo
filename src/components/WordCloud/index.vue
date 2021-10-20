2becdd6e6a365d5595864e0d05939e17c8d38d30
964269669 <964269669@qq.com>
2021-10-20 20:00:37 +0800

    v-charts(ve-wordcloud)完成词云图组件

--- src/components/WordCloud/index.vue
+++ src/components/WordCloud/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    222
+    <ve-wordcloud :data="chartData" :settings="chartSettings" height="100%"></ve-wordcloud>
   </div>
 </template>
 
@@ -8,7 +8,60 @@
 export default {
   name: 'wordCloud',
   data () {
-    return {}
+    return {
+      chartData: {
+        columns: ['name', 'value'],
+        rows: [
+          {
+            name: '马超',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超1',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超2',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超3',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超4',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超5',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超6',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超7',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超8',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超9',
+            value: 100 * Math.random()
+          },
+          {
+            name: '马超10',
+            value: 100 * Math.random()
+          }
+        ]
+      },
+      chartSettings: {
+        color: ['rgba(97,216,0,.7)', 'rgba(204,178,26,.7)', 'rgba(245,166,35,.7)', 'rgba(156,13,113,.7)']
+      }
+    }
   },
   created () {},
   mounted () {},
@@ -16,6 +69,8 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+}
 </style>
