e67a82ca1011acc10ed05a8fea9a9287a6f08902
964269669 <964269669@qq.com>
2021-10-21 20:25:42 +0800

    销售额、访问量、排行榜接入api数据

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -54,8 +54,11 @@
 </template>
 
 <script>
+import commonDatadMixin from '@/mixins/commonDataMixin'
+
 export default {
   name: 'salesView',
+  mixins: [commonDatadMixin],
   data () {
     return {
       activeIndex: '1',
@@ -105,7 +108,20 @@ export default {
         },
         xAxis: {
           type: 'category',
-          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+          data: [
+            '1月',
+            '2月',
+            '3月',
+            '4月',
+            '5月',
+            '6月',
+            '7月',
+            '8月',
+            '9月',
+            '10月',
+            '11月',
+            '12月'
+          ],
           axisTick: {
             alignWithLabel: true,
             lineStyle: {
@@ -149,44 +165,49 @@ export default {
           right: 60,
           bottom: 50
         }
-      },
-      rankData: [
-        {
-          no: 1,
-          name: '麦当劳',
-          money: '323,234'
-        },
-        {
-          no: 2,
-          name: '麦当劳',
-          money: '323,234'
-        },
-        {
-          no: 3,
-          name: '麦当劳',
-          money: '323,234'
-        },
-        {
-          no: 4,
-          name: '麦当劳',
-          money: '323,234'
-        },
-        {
-          no: 5,
-          name: '麦当劳',
-          money: '323,234'
-        },
-        {
-          no: 6,
-          name: '麦当劳',
-          money: '323,234'
-        },
-        {
-          no: 7,
-          name: '麦当劳',
-          money: '323,234'
-        }
-      ]
+      }
+      // rankData: [
+      //   {
+      //     no: 1,
+      //     name: '麦当劳',
+      //     money: '323,234'
+      //   },
+      //   {
+      //     no: 2,
+      //     name: '麦当劳',
+      //     money: '323,234'
+      //   },
+      //   {
+      //     no: 3,
+      //     name: '麦当劳',
+      //     money: '323,234'
+      //   },
+      //   {
+      //     no: 4,
+      //     name: '麦当劳',
+      //     money: '323,234'
+      //   },
+      //   {
+      //     no: 5,
+      //     name: '麦当劳',
+      //     money: '323,234'
+      //   },
+      //   {
+      //     no: 6,
+      //     name: '麦当劳',
+      //     money: '323,234'
+      //   },
+      //   {
+      //     no: 7,
+      //     name: '麦当劳',
+      //     money: '323,234'
+      //   }
+      // ]
+    }
+  },
+  computed: {
+    rankData () {
+      return this.activeIndex === '1' ? this.orderRank : this.userRank
     }
   },
   created () {},
@@ -194,11 +215,11 @@ export default {
   methods: {
     onMenuSelect (index) {
       this.activeIndex = index
-      // if (index === '1') {
-      //   this.render(this.orderFullYear, this.orderFullYearAxis, '年度销售额')
-      // } else {
-      //   this.render(this.userFullYear, this.userFullYearAxis, '年度用户访问量')
-      // }
+      if (index === '1') {
+        this.render(this.orderFullYear, this.orderFullYearAxis, '年度销售额')
+      } else {
+        this.render(this.userFullYear, this.userFullYearAxis, '年度用户访问量')
+      }
     },
     render (data, axis, title) {
       this.chartOption = {
@@ -261,9 +282,9 @@ export default {
     }
   },
   watch: {
-    // orderFullYear () {
-    //   this.render(this.orderFullYear, this.orderFullYearAxis, '年度销售额')
-    // }
+    orderFullYear () {
+      this.render(this.orderFullYear, this.orderFullYearAxis, '年度销售额')
+    }
   }
 }
 </script>
